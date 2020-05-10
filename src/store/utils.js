e592d255ad9ef7845b5a8ce0ed6f6627ab4f5a1c
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2020-05-10 14:36:04 +0300

    Dev.new (#62)
    * DEV.NEW
    
    - update vue
    - refactoring
    
    * DEV.NEW
    
    - fix: file location
    - add: actions
    
    * DEV.NEW
    
    - add: modal
    
    * DEV.NEW
    
    - PR fix

--- src/store/utils.js
+++ src/store/utils.js
@@ -1,16 +1,14 @@
 export const getChild = (childs, path) => {
-    const currentItem = childs.find(child => child.hash === path[0])
+  const currentItem = childs.find(child => child.hash === path[0])
 
-    if (path.length === 1) {
-        return currentItem
-    }
-    return getChild(currentItem.childs, path.slice(1))
+  if (path.length === 1) {
+    return currentItem
+  }
+  return getChild(currentItem.childs, path.slice(1))
 }
 
 export const getHash = () => {
-    return Date.now() +  '_' + Math.random().toString(36).substr(2, 9);
+  return Date.now() + '_' + Math.random().toString(36).substr(2, 9);
 }
 
 export const hexGen = () => `#${Math.random().toString(16).slice(2, 8)}`
-
-
